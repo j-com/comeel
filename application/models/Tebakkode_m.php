6ad34bdb7a37b267d89be0bef0f1bc8707386a22
usmanantharikta <antharikt7@gmail.com>
2017-03-09 05:21:03 +0700

    sadadasd

---
+++ application/models/Tebakkode_m.php
@@ -0,0 +1,33 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Tebakkode_m extends CI_Model {
+
+  function __construct(){
+    parent::__construct();
+    $this->load->database();
+  }
+
+  function log_events($signature, $body)
+  {
+    $this->db->set('signature', $signature)
+    ->set('events', $body)
+    ->insert('eventlog');
+
+    return $this->db->insert_id();
+  }
+
+  function getUser($userId)
+{
+    $data = $this->db->where('user_id', $userId)   ->get('users')->row_array();
+    if(count($data) > 0)    return $data;
+    return false;
+}
+
+function saveUser($profile)
+{
+    $this->db->set('user_id', $profile['userId'])
+    ->set('display_name', $profile['displayName'])
+    ->insert('users');
+    return $this->db->insert_id();
+}
+}
